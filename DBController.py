451ee86d2a2f43266a5af729460edaf273ba92fe
exsonic <pubobi.china@gmail.com>
2013-06-25 11:05:50 -0700

    add functions

--- DBController.py
+++ DBController.py
@@ -7,9 +7,49 @@ from pymongo import MongoClient
 class DBController(object):
 	def __init__(self):
 		try:
-			# self.db = self.client.test
-			self._db = MongoClient().ChemCorpus
+			self._db = MongoClient().DialogueCorpus
 		except Exception as e:
 			print e
 			exit()
 
+	def dropConference(self):
+		self._db.conference.drop()
+
+	def dropSession(self):
+		self._db.session.drop()
+
+	def dropSpeech(self):
+		self._db.speech.drop()
+
+	def insertConference(self, conference):
+		conference['_id'] = str(self._db.conference.count())
+		self._db.conference.insert(conference)
+		return conference
+
+	def insertSession(self, session):
+		session['_id'] = str(self._db.session.count())
+		self._db.session.insert(session)
+		return session
+
+	def insertSpeech(self, speech):
+		speech['_id'] = str(self._db.speech.count())
+		self._db.speech.insert(speech)
+		return speech
+
+	def getConferenceByCompanyTime(self, company, time):
+		return self._db.conference.find_one({'company' : company, 'time' : time})
+
+	def getSessionByConferenceAndOrder(self, conferenceId, order):
+		return self._db.session.find_one({'conference' : conferenceId, 'order' : order})
+
+	def getSpeechByConferenceIdAndSessionIdAndOrder(self, conferenceId, sessionId, sessionOrder):
+		return self._db.session.find_one({'conference' : conferenceId, 'session' : sessionId, 'order' : sessionOrder})
+
+	def getConferenceById(self, conferenceId):
+		return self._db.conference.find_one({'_id' : conferenceId})
+
+	def getSessionById(self, sessionId):
+		return self._db.session.find_one({'_id' : sessionId})
+
+	def getSpeechById(self, speechId):
+		return self._db.speech.find_one({'_id' : speechId})
--- FileUtils.py
+++ FileUtils.py
@@ -1,6 +1,56 @@
 """
 Bobi Pu, bobi.pu@usc.edu
 """
+from DBController import DBController
+import os
 
-def loadAllDialoguesFromFile(filePath):
-	pass
+def loadAllDialoguesFromFile(folderPath):
+	db = DBController()
+	db.dropConference()
+	db.dropSession()
+	db.dropSpeech()
+	for dirPath, dirNames, fileNames in os.walk(folderPath):
+		print(dirPath)
+		if dirPath.split('/')[-1].startswith('chunk'):
+			for fileName in fileNames:
+				try:
+					if fileName.endswith('txt'):
+						fileNameParts = [part.strip() for part in fileName.split('.txt')[0].split('_')]
+						company, time = fileNameParts[0], fileNameParts[1]
+						sessionType, sessionOrder, asker, answerer = fileNameParts[2], int(fileNameParts[3]), fileNameParts[4], fileNameParts[5]
+						if fileNameParts[-1].endswith('default') or fileNameParts[-1].endswith('copy'):
+							continue
+						elif fileNameParts[-1][-1].isdigit() and not fileNameParts[-1][-2].isdigit():
+							speaker = fileNameParts[-1][:-1]
+							speechOrder = int(fileNameParts[-1][-1:])
+						elif fileNameParts[-1][-1].isdigit() and fileNameParts[-1][-2].isdigit():
+							speaker = fileNameParts[-1][:-2]
+							speechOrder = int(fileNameParts[-1][-2:])
+						else:
+							continue
+						conference =  db.getConferenceByCompanyTime(company, time)
+						if conference is None:
+							conference = {'company' : company, 'time' : time}
+							conference = db.insertConference(conference)
+
+						session = db.getSessionByConferenceAndOrder(conference['_id'], sessionOrder)
+						if session is None:
+							session = {'conference' : conference['_id'], 'order' : speechOrder, 'type' : sessionType, 'asker' : asker, 'answerer' : answerer}
+							session = db.insertSession(session)
+
+						speech = db.getSpeechByConferenceIdAndSessionIdAndOrder(conference['_id'], session['_id'], speechOrder)
+						if speech is None:
+							filePath = dirPath + '/' + fileName
+							with open(filePath, 'r') as f:
+								text = ' '.join(f.readlines()).strip()
+								text = text.decode('ascii', 'ignore').encode('ascii', 'ignore')
+								speech = {'conference' : conference['_id'], 'session' : session['_id'], 'order' : speechOrder, 'speaker' : speaker, 'text' : text}
+								db.insertSpeech(speech)
+				except Exception as e:
+					print(fileName)
+					print(e)
+
+
+
+if __name__ == '__main__':
+	loadAllDialoguesFromFile('/Users/exsonic/Developer/Marshall_RA/chunk_done/')
